2112854479236ffde3a36ef247332ea029eef57d
Gladstone Rose <gladstonerose@hotmail.com>
2023-07-27 00:27:38 -0500

    modified script.js and changed piture files

--- script.js
+++ script.js
@@ -19,49 +19,24 @@ document.addEventListener("DOMContentLoaded", function() {
             description: "Our carefully crafted Dresser is a luxurious addition to any living space. Its sleek, contemporary design and style offers aesthetic beauty to any space it occupies.",
             image: "dresser-spread-eagles.jpeg",
         },
-
         {
-            name: "Dining Room Table",
-            price: "J$299,000.00",
-            description: "Meticulously crafted from the finest, hand-selected materials, this stunning dining room table promises to elevate your dining experience to new heights. With its sleek, polished surface, the table becomes an enchanting focal point, captivating both guests and family members alike.",
-            image: "table2.jpg",
+            name: "Dresser with 2 night stands",
+            price: "J$110,000.00",
+            description: "Our carefully crafted Dresser is a luxurious addition to any living space. Its sleek, contemporary design and style offers aesthetic beauty to any space it occupies.",
+            image: "dresser-2-night-stands.jpeg",
+        },
+        {
+            name: "King Size Bed with a pair of night stands",
+            price: "J$185,000.00",
+            description: "Experience the ultimate comfort and relaxation with our cozy bed. Crafted with the finest materials, this bed will provide you with a good night's sleep like never before.",
+            image: "bed-with-2-night-stand.jpeg",
         },
 
-        {
-            name: "Elegant Breakfront",
-            price: "J$194,850.00",
-            description: "Enhance the beauty and functionality of your living space with our elegant breakfront. Display your treasured items and store your essentials in style with this timeless piece.",
-            image: "breakfront.jpg",
-        },
-        {
-            name: "Classic Bookcase",
-            price: "J$89,850.00",
-            description: "Organize your books and show off your literary collection with our classic bookcase. This bookcase blends style and practicality to elevate any room.",
-            image: "bookcase.jpg",
-        },
-        {
-            name: "Plush Rocking Chair",
-            price: "J$44,850.00",
-            description: "Indulge in pure relaxation with our plush rocking chair. Perfect for unwinding after a long day, this chair will become your favorite spot in the house.",
-            image: "rocking.webp",
-        },
-        {
-            name: "Elegant Sofa",
-            price: "J$74,850.00",
-            description: "A comfortable and stylish sofa for your living room. Relax in luxury and add a touch of elegance to your home décor.",
-            image: "sofa.webp",
-        },
         {
             name: "Chest of Drawers",
             price: "J$119,850.00",
-            description: "Experience the ultimate comfort and relaxation with our cozy bed. Crafted with the finest materials, this bed will provide you with a good night's sleep like never before.",
+            description: "This chest-of-drawers does exactly what it promises.",
             image: "chest-of-drawers-spread-eagles.jpeg",
-        },
-        {
-            name: "Cozy Bed",
-            price: "J$119,850.00",
-            description: "Experience the ultimate comfort and relaxation with our cozy bed. Crafted with the finest materials, this bed will provide you with a good night's sleep like never before.",
-            image: "Bed-spread-eagles.jpeg"
         },
 
         // Add more furniture items here as needed
